beb04a087b204118ec312a6f116cacb1a9b14dbf
Chris <cjacocks@daltonstate.edu>
2023-05-01 16:01:14 -0400

    Renamed shit

--- src/main/java/com/example/demo/TicTacToeGameScreenController.java
+++ src/main/java/com/example/demo/TicTacToeGameScreenController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class GameScreenController {
+public class TicTacToeGameScreenController {
 
     @FXML
     private Button Button1;
