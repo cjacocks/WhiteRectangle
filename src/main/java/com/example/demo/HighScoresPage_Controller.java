9fa3ac0d185e3aaeed2f0cf3a2a71c230fe529ec
grivera5 <111932733+grivera5@users.noreply.github.com>
2023-05-01 21:59:04 -0400

    Create HighScoresPage_Controller.java

---
+++ src/main/java/com/example/demo/HighScoresPage_Controller.java
@@ -0,0 +1,29 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HighScoresPage_Controller {
+
+    @FXML
+    private Button BackToMainMenuButton;
+
+    @FXML
+    private void handleBackToMainMenuButtonClick() {
+        Stage currentStage = (Stage) BackToMainMenuButton.getScene().getWindow();
+        currentStage.close();
+
+        try {
+            Parent mainMenuPage = FXMLLoader.load(getClass().getResource("/com/example/demo/MainMenuInitialPage.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(mainMenuPage));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
