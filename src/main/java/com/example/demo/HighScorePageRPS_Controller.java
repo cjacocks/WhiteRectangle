051d3882384dd2d4b54bb5777ced585f9e91fe0d
Chris <cjacocks@daltonstate.edu>
2023-05-03 23:15:21 -0400

    Added more High Score Things. Just need to Make Table go BRRRT

--- src/main/java/com/example/demo/HighScorePageRPS_Controller.java
+++ src/main/java/com/example/demo/HighScorePageRPS_Controller.java
@@ -1,4 +1,49 @@
 package com.example.demo;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HighScorePageRPS_Controller {
+    // TODO -- Make Table go brrr
+    @FXML
+    private Button mainMenuButton;
+
+    @FXML
+    private Button highScoreButton;
+
+    @FXML
+    private void handleMainMenuButtonClick() {
+        Stage currentStage = (Stage) mainMenuButton.getScene().getWindow();
+        currentStage.close();
+
+        try {
+            Parent mainMenuPage = FXMLLoader.load(getClass().getResource("/com/example/demo/MainMenuInitialPage.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(mainMenuPage));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleHighScoreButtonClick() {
+        Stage currentStage = (Stage) highScoreButton.getScene().getWindow();
+        currentStage.close();
+
+        try {
+            Parent mainMenuPage = FXMLLoader.load(getClass().getResource("/com/example/demo/HighScoresPage.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(mainMenuPage));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
