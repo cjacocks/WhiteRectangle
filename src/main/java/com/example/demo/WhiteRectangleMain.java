6ae6cbc6f25dd6f8b3141157dcab40f266dd86c9
Chris <cjacocks@daltonstate.edu>
2023-05-04 01:28:03 -0400

    Another attempt at merging. My sanity is not dwindling. It is gone.

--- src/main/java/com/example/demo/WhiteRectangleMain.java
+++ src/main/java/com/example/demo/WhiteRectangleMain.java
@@ -6,16 +6,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.io.File;
 import java.io.IOException;
+
 
 public class WhiteRectangleMain extends Application {
     /*
     This class file should serve as the primary controller for all other classes. It will launch the game menu
     and once a game is selected, will initiate any functions or methods needed to serve the player with that game.
 
-    This class may not even be needed. TBD
      */
 
+    private static final String DATABASE_NAME = "high_score_database.db";
     @Override
     public void start (Stage stage) throws IOException {
         /*
@@ -38,7 +45,48 @@ public class WhiteRectangleMain extends Application {
 
 
     public static void main(String[]args) {
+        try {
+            File file = new File(DATABASE_NAME);
+            if (!file.exists()) {
+                System.out.println("WhiteRectangle: Database not initialized, initializing a new DB.");
+                setupDatabase();
+            } else {
+                System.out.println("WhiteRectangle: Database already present, proceeding.");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error setting up DB: " + e.getMessage());
+        }
+
         System.out.println("WhiteRectangle: Running Main Method Now");
         launch();
+    }
+
+    private static void setupDatabase() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_NAME);
+
+        // Create DiceGame highscores table
+        String diceGameSql = "CREATE TABLE dice_game_highscores ("
+                + "id INTEGER PRIMARY KEY,"
+                + "date TEXT,"
+                + "score REAL"
+                + ")";
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute(diceGameSql);
+        }
+
+        // Create Rock Paper Scissors game table
+        String rpsGameSql = "CREATE TABLE rps_game ("
+                + "id INTEGER PRIMARY KEY,"
+                + "date TEXT,"
+                + "player1 INT,"
+                + "player2 INT,"
+                + "result INT"
+                + ")";
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute(rpsGameSql);
+        }
+
+        // Close database connection
+        connection.close();
     }
 }
